23e05d3589acca7c246f66d43794d39258ad45cf
Arikaim <info@arikaim.com>
2022-12-02 20:40:23 +0200

    =

---
+++ Services.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c)  Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Extensions\Services;
+
+use Arikaim\Core\Extension\Extension;
+
+/**
+ * Services extension
+*/
+class Services extends Extension
+{
+    /**
+     * Install extension routes, events, jobs
+     *
+     * @return void
+    */
+    public function install()
+    {         
+    }   
+
+    /**
+     * UnInstall extension
+     *
+     * @return void
+    */
+    public function unInstall()
+    {  
+    }
+}
